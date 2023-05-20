04aae30a39eebc8fdc9864f587ec4ab4a512edf8
Tyler Moyer <codeguyty@icloud.com>
2023-05-20 18:55:20 -0400

    Added contact.js and Animate On Scroll effects

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React from 'react';
 import Card from './components/Card';
 import About from './components/About';
 import Skills from './components/Skills';
+import ContactMe from './components/Contact';
 import Footer from './components/Footer';
-
-
+import AOS from 'aos';
+import { useEffect } from 'react';
+import 'aos/dist/aos.css';
 
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+        
+    })
+  })
+
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-      <Card />
-      <About />
-      <Skills />
-      <Footer />
+      <div data-aos="fade-down" data-aos-duration="800">
+        <Card />
+      </div>
+      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+        <About />
+      <div data-aos="flip-left" data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000">
+        <Skills />
+      </div>
+        <ContactMe />
+        <Footer />
+
+      </div>
     </div>
   );
 }
